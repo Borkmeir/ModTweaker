a73db9232b387efb754b0126e6c460a9a44d4544
joshiejack <joshjackwildman@gmail.com>
2014-06-29 06:59:08 +0100

    Readded binding recipes

--- src/main/java/modtweaker/bloodmagic/BloodMagic.java
+++ src/main/java/modtweaker/bloodmagic/BloodMagic.java
@@ -5,6 +5,7 @@ import minetweaker.MineTweakerAPI;
 public class BloodMagic {
 	public BloodMagic() {
 		MineTweakerAPI.registerClass(Alchemy.class);
+		MineTweakerAPI.registerClass(Binding.class);
 		MineTweakerAPI.registerClass(BloodAltar.class);
 	}
 }
